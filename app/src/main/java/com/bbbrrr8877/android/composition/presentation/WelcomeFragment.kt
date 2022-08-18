c366ef4eb5a661e0e6b142b1359bfeed403efefc
Barbusya <bbbrrr8877@gmail.com>
2022-08-19 00:51:00 +0300

    FragmentManager onBackPressedDispatcher popBackStack

--- app/src/main/java/com/bbbrrr8877/android/composition/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/bbbrrr8877/android/composition/presentation/WelcomeFragment.kt
@@ -23,8 +23,22 @@ class WelcomeFragment : Fragment() {
         return binding.root
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonUnderstand.setOnClickListener {
+            launchChooseLevelFragment()
+        }
+    }
+
+    private fun launchChooseLevelFragment() {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, ChooseLevelFragment.newInstance())
+            .addToBackStack(ChooseLevelFragment.NAME)
+            .commit()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
 }
